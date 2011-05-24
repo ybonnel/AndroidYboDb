e33cd081470ba98a64c43bc43e4f59040c7f69b4
ybonnel <ybonnel@gmail.com>
2011-05-24 17:43:42 +0200

    Complete exercise one.

--- Notepadv1/src/com/android/demo/notepad1/Notepadv1.java
+++ Notepadv1/src/com/android/demo/notepad1/Notepadv1.java
@@ -16,10 +16,13 @@
 
 package com.android.demo.notepad1;
 
-import android.app.Activity;
+import com.android.demo.notepad1.modele.Note;
+
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 /**
  * This is the adaptation of the notepad exemple of android, to show how to use AndroidYboDb.
@@ -27,23 +30,52 @@ import android.view.MenuItem;
  * @author ybonnel
  *
  */
-public class Notepadv1 extends Activity {
+public class Notepadv1 extends ListActivity {
+	
+	private NotesDbAdapter mDbHelper;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.notepad_list);
+        mDbHelper = new NotesDbAdapter(this);
+        fillData();
     }
+    
+    public static final int INSERT_ID = Menu.FIRST;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Auto-generated method stub
-        return super.onCreateOptionsMenu(menu);
+    	boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+        return result;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        return super.onOptionsItemSelected(item);
+    	 switch (item.getItemId()) {
+         case INSERT_ID:
+             createNote();
+             return true;
+         }
+        
+         return super.onOptionsItemSelected(item);
+    }
+    
+    private int mNoteNumber = 1;
+    
+    private void createNote() {
+    	Note note = new Note();
+    	note.title = "Note " + mNoteNumber++;
+    	note.body = "";
+        mDbHelper.insert(note);
+        fillData();
+    }
+    
+    private void fillData() {
+        // Now create an array adapter and set it to display using our row
+    	// FIXME add selectAll feature.
+        setListAdapter(new ArrayAdapter<Note>(this, R.layout.notes_row, mDbHelper.select(new Note())));
     }
 }
